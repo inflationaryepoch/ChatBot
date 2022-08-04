cee891c8ff76273f05e3014d5e9effab4152cd7e
Enes Erdil <86965500+inflationaryepoch@users.noreply.github.com>
2022-08-04 18:24:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def yardim(update, context):
 
 
 def iletisim(update, context):
-    update.message.reply_text('📍 NEF 22 Ataköy/İstanbul\n\n📞 +905306480734\n\n📩 WhatsApp hattımız\n <i>text</i>')
+    update.message.reply_text('📍 NEF 22 Ataköy/İstanbul\n\n📞 +905306480734\n\n📩 WhatsApp hattımız\n <i>text</i>**bold**')
 
 
 def hizmetler(update, context):
